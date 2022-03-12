7fb4ae9f988a1734eee975a94f4f319b1ef43a00
lapwasss <titou-10@hotmail.fr>
2022-03-12 22:08:47 +0100

    C00-ex01

--- ft_print_alphabet/ft_print_alphabet.c
+++ ft_print_alphabet/ft_print_alphabet.c
@@ -6,7 +6,7 @@
 /*   By: Antoine <antoine@notstudent.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 18:26:35 by antoine           #+#    #+#             */
-/*   Updated: 2022/03/12 21:15:40 by Antoine          ###   ########.fr       */
+/*   Updated: 2022/03/12 22:07:37 by Antoine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ void	ft_print_alphabet(void)
 {
 	char	c;
 
-	c = '`';
-	while (c != 'z')
+	c = 'a';
+	while (c <= 'z')
 	{
-		c++;
 		ft_putchar(c);
+		c++;
 	}
 }
